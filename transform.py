031990f45312dd9fe81a6d3772a23bcec315edcb
Randy158 <15862799388@163.com>
2025-10-20 15:13:16 +0800

    Update transform.py

--- transform.py
+++ transform.py
@@ -1,1 +1,34 @@
 
+
+from albumentations.pytorch import ToTensorV2
+from albumentations import *
+from albumentations.augmentations import transforms
+from albumentations.core.composition import Compose, OneOf
+from albumentations import RandomRotate90, Resize, HorizontalFlip, RandomBrightnessContrast, GaussianBlur
+
+transform = {
+        'train': Compose([
+            HorizontalFlip(p=0.5),
+            #RandomBrightnessContrast(p=0.2),
+            #GaussianBlur(p=0.1),
+            Resize(height=256, width=256, p=1.0),
+            transforms.Normalize(),
+            ToTensorV2()
+        ]),
+        'val': Compose([
+            Resize(height=256, width=256, p=1.0),
+            transforms.Normalize(),
+            ToTensorV2()
+        ]),
+        'test': Compose([
+            Resize(height=256, width=256, p=1.0),
+            transforms.Normalize(),
+            ToTensorV2()
+        ])
+}
+
+
+
+
+
+
