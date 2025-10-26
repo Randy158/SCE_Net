1ef9836b0fa1e19827af1056ba49cd2c1b957753
Randy158 <15862799388@163.com>
2025-10-26 13:37:36 +0800

    Update train.py

--- train.py
+++ train.py
@@ -11,13 +11,11 @@ from Train_model import train
 if __name__ == "__main__":
     
 
-    train_img_dir = r"Datasets/train/images"
-    train_label_dir = r"Datasets/train/labels"
+    train_img_dir = ""
+    train_label_dir = ""
+    val_img_dir = ""
+    val_label_dir = ""
 
-    val_img_dir = r"Datasets/val/images"
-    val_label_dir = r"Datasets/val/labels"
-
-    
     epoch = 200
     classes_num = 2
     log_dir = r"logs"
@@ -29,10 +27,9 @@ if __name__ == "__main__":
 
     train_data = MedSegment(train_img_dir, train_label_dir, transform['train'])
     train_dataloader = DataLoader(train_data, batch_size=8, shuffle=True, num_workers=4, drop_last=True)
-
     val_data = MedSegment(val_img_dir, val_label_dir, transform['val'])
     val_dataloader = DataLoader(val_data, batch_size=8, shuffle=False, num_workers=4, drop_last=False)
-    net = DL_Net(num_classes=classes_num)
+    net = SCE_Net(num_classes=classes_num)
 
     optimizer = torch.optim.Adam(net.parameters(), lr=1e-4, weight_decay=1e-4)
     lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=400, eta_min=1e-5)
