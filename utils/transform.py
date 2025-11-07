2bb8ebb9ee0077f3f66af9b7bc7bffb29a408813
Randy158 <15862799388@163.com>
2025-11-07 09:20:00 +0800

    Update transform.py

--- utils/transform.py
+++ utils/transform.py
@@ -7,13 +7,16 @@ from albumentations import RandomRotate90, Resize, HorizontalFlip, RandomBrightn
 transform = {
         'train': Compose([
             HorizontalFlip(p=0.5),
-            #RandomBrightnessContrast(p=0.2),
-            #GaussianBlur(p=0.1),
+            RandomBrightnessContrast(p=0.2),
+            GaussianBlur(p=0.1),
             Resize(height=256, width=256, p=1.0),
             transforms.Normalize(),
             ToTensorV2()
         ]),
         'val': Compose([
+            #HorizontalFlip(p=0.5),
+            #RandomBrightnessContrast(p=0.2),
+            #GaussianBlur(p=0.1),
             Resize(height=256, width=256, p=1.0),
             transforms.Normalize(),
             ToTensorV2()
