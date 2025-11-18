72c7d6d89c4afd64343629b43e7ee59479449922
Randy158 <15862799388@163.com>
2025-11-18 10:07:23 +0800

    Update build_resnet.py

--- Resnet/build_resnet.py
+++ Resnet/build_resnet.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from .basic_module import BasicBlock, Bottleneck, BN_MOMENTUM
+from .core import BasicBlock, Bottleneck, BN_MOMENTUM
 
 import torch.nn as nn
 import torch.utils.model_zoo as model_zoo
