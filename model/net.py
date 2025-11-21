73c65053d6c7f124ed487ab556807f7e234fc8e7
Randy158 <15862799388@163.com>
2025-11-21 10:45:50 +0800

    Update net.py

--- model/net.py
+++ model/net.py
@@ -61,43 +61,49 @@ class SCE_Net(nn.Module):
     def __init__(self, num_classes=1):
         super().__init__()
         filters = [64, 128, 256, 512]
-        resnet = create_resnet('resnet34', pretrain=True)
-        self.firstconv = resnet.conv1
-        self.firstbn = resnet.bn1
-        self.firstrelu = resnet.relu
-        self.firstmaxpool = resnet.maxpool
-        self.encoder1 = resnet.layer1
-        self.encoder2 = resnet.layer2
-        self.encoder3 = resnet.layer3
-        self.encoder4 = resnet.layer4
+        
+        # Initialize ResNet34 backbone
+        resnet_backbone = create_resnet('resnet34', pretrain=True)
+        self.initial_conv = resnet_backbone.conv1
+        self.initial_bn = resnet_backbone.bn1
+        self.initial_relu = resnet_backbone.relu
+        self.initial_maxpool = resnet_backbone.maxpool
+       
+        self.encoder_stage1 = resnet_backbone.layer1
+        self.encoder_stage2 = resnet_backbone.layer2
+        self.encoder_stage3 = resnet_backbone.layer3
+        self.encoder_stage4 = resnet_backbone.layer4
     
-        self.decoder4 = DecoderBlock(512, 256, 256)  
-        self.decoder3 = DecoderBlock(256, 128, 128)  
-        self.decoder2 = DecoderBlock(128, 64, 64)  
-        self.decoder1 = nn.Sequential(
+        self.decoder_stage4 = DecoderBlock(512, 256, 256)  
+        self.decoder_stage3 = DecoderBlock(256, 128, 128)  
+        self.decoder_stage2 = DecoderBlock(128, 64, 64)  
+        self.decoder_stage1 = nn.Sequential(
             nn.Conv2d(64, 64, 3, padding=1),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
         )
-        self.finaldeconv1 = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
-        self.finalrelu1 = nonlinearity
-        self.finalconv2 = nn.Conv2d(32, 32, 3, padding=1)
-        self.finalrelu2 = nonlinearity
-        self.finalconv3 = nn.Conv2d(32, num_classes, 3, padding=1)
+        
+        self.final_upconv1  = nn.ConvTranspose2d(filters[0], 32, 4, 2, 1)
+        self.final_relu1 = nonlinearity
+        self.final_conv2 = nn.Conv2d(32, 32, 3, padding=1)
+        self.final_relu2 = nonlinearity
+        self.final_conv3 = nn.Conv2d(32, num_classes, 3, padding=1)
+        
         self.conv = nn.Conv2d(1, 3, kernel_size=1, stride=1)
         self.vssm_encoder = VSSE(patch_size=2, in_chans=48)
+        # initial feature extraction
         self.stem = nn.Sequential(
             nn.Conv2d(3, 48, kernel_size=7, stride=2, padding=3),
             nn.InstanceNorm2d(48, eps=1e-5, affine=True),
         )
 
-        self.fusion1 = DynamicFusion(64)
-        self.fusion2 = DynamicFusion(128)
-        self.fusion3 = DynamicFusion(256)
-        self.fusion4 = DynamicFusion(512)
+        self.fusion_stage1 = DynamicFusion(64)
+        self.fusion_stage2 = DynamicFusion(128)
+        self.fusion_stage3 = DynamicFusion(256)
+        self.fusion_stage4 = DynamicFusion(512)
  
-        self.edge3 = EdgeAttention(512)
-        self.edge2 = EdgeAttention(256)
+        self.edge_attention3 = EdgeAttention(512)
+        self.edge_attention2 = EdgeAttention(256)
    
         self.vss_pre_cnn = nn.Sequential(
             nn.Conv2d(48, 48, kernel_size=3, padding=1),
@@ -134,37 +140,39 @@ class SCE_Net(nn.Module):
         ])
 
     def forward(self, x):
-        ssmx = self.stem(x)  # [B, 48, H/2, W/2]
+        stem_output = self.stem(x)  # [B, 48, H/2, W/2]
         # Before VSS
-        ssmx = self.vss_pre_cnn(ssmx)  # [16, 48, 128, 128]
+        stem_output = self.vss_pre_cnn(stem_output)  
         # VSSM Encoder
-        vssm_feats = self.vssm_encoder(ssmx)  # [B, C, H, W]
+        vssm_features = self.vssm_encoder(stem_output)  # [B, C, H, W]
         # After VSS
-        vssm_feats = [self.vss_post_cnn[i](vssm_feats[i]) for i in range(5)]
-        # v1~v4
-        v0, v1, v2, v3, v4 = vssm_feats[0:5]
-        x = self.firstconv(x)
-        x = self.firstbn(x)
-        x = self.firstrelu(x)
-        x = self.firstmaxpool(x)
-        e1 = self.encoder1(x)
-        e2 = self.encoder2(e1)
-        e3 = self.encoder3(e2)
-        e4 = self.encoder4(e3)
+        vssm_features = [self.vss_post_cnn[i](vssm_features[i]) for i in range(5)]
+        v0, v1, v2, v3, v4 = vssm_features[0:5]
+        x = self.initial_conv(x)
+        x = self.initial_bn(x)
+        x = self.initial_relu(x)
+        x = self.initial_maxpool(x)
+        e1 = self.encoder_stage1(x)
+        e2 = self.encoder_stage2(e1)
+        e3 = self.encoder_stage3(e2)
+        e4 = self.encoder_stage4(e3)
+        
         # Dynamic fusion    
-        e1 = self.fusion1(e1, v1)
-        e2 = self.fusion2(e2, v2)
-        e3 = self.fusion3(e3, v3)
-        e4 = self.fusion4(e4, v4)
+        e1 = self.fusion_stage1(e1, v1)
+        e2 = self.fusion_stage2(e2, v2)
+        e3 = self.fusion_stage3(e3, v3)
+        e4 = self.fusion_stage4(e4, v4)
         # Decoder
-        d4 = self.decoder4(self.edge3(e4), e3)
-        d3 = self.decoder3(self.edge2(d4), e2)
-        d2 = self.decoder2(d3, e1)
-        d1 = self.decoder1(d2)
-        out = self.finaldeconv1(d1)
-        out = self.finalrelu1(out)
-        out = self.finalconv2(out)
-        out = self.finalrelu2(out)
-        out = self.finalconv3(out)
+        d4 = self.decoder_stage4(self.edge3(e4), e3)
+        d3 = self.decoder_stage3(self.edge2(d4), e2)
+        d2 = self.decoder_stage2(d3, e1)
+        d1 = self.decoder_stage1(d2)
+        
+        out = self.final_upconv1(d1)
+        out = self.final_relu1(out)
+        out = self.final_conv2(out)
+        out = self.final_relu2(out)
+        out = self.final_conv3(out)
+        
         out = F.interpolate(out, size=(256, 256), mode='bilinear', align_corners=False)
         return torch.sigmoid(out)
