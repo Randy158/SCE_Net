739787764723d5653a51f5a52cd385f1a4fceb26
Randy158 <15862799388@163.com>
2025-11-21 10:57:02 +0800

    Update DataLoade.py

--- utils/DataLoade.py
+++ utils/DataLoade.py
@@ -2,26 +2,38 @@ from torch.utils.data import Dataset
 import os
 import numpy as np
 from PIL import Image
+from pathlib import Path
 
 class MedSegment(Dataset):
-    def __init__(self, img_dir, label_dir, transform=None, image_suffix: str = '.jpg',label_suffix: str = '.png'):
+    def __init__(self, img_dir, label_dir, transform=None, image_suffix='.jpg', label_suffix='.png'):
         self.img_dir = img_dir
         self.label_dir = label_dir
         self.transform = transform
         self.image_suffix = image_suffix
         self.label_suffix = label_suffix
-        self.img_name = os.listdir(img_dir)
 
+        self.img_files = [f for f in os.listdir(img_dir) if f.endswith(self.image_suffix)]
+        
+        self.label_files = {Path(f).stem: f for f in os.listdir(label_dir) if f.endswith(self.label_suffix)}
+
+        self.img_name = [
+            img for img in self.img_files if Path(img).stem in self.label_files
+        ]
+        
     def __len__(self):
         return len(self.img_name)
 
     def __getitem__(self, index):
-        image = np.array(Image.open(os.path.join(self.img_dir, self.img_name[index])).convert("RGB"))
-        label = np.array(Image.open(os.path.join(self.label_dir, self.img_name[index].replace(self.image_suffix, self.label_suffix)))).astype(np.float16)
+        img_name = self.img_name[index]
+        img_path = os.path.join(self.img_dir, img_name)
+        label_path = os.path.join(self.label_dir, self.label_files[Path(img_name).stem])
 
-        if self.transform is not None:
+       
+        image = np.array(Image.open(img_path).convert("RGB"))
+        label = np.array(Image.open(label_path)).astype(np.float32)  # 标签转换为 float32 或 np.uint8
+
+        if self.transform:
             res = self.transform(image=image, mask=label)
+            image, label = res['image'], res['mask']
 
-        return res
-
-
+        return image, label
